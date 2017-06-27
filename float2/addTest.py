f952def6e65b91f636cd05ceb9ae5ae9deece9bb
mojdeh <robati.m7@gmail.com>
2017-06-28 01:53:37 +0430

    ok

---
+++ float2/addTest.py
@@ -0,0 +1,23 @@
+from float2 import *
+
+print("addition")        
+a=Float([9,2,7,7],1)
+b=Float([2,3,4,5],1)
+c=a+b
+assert str(c)=="+1162.2"
+print("\naddition") 
+a=Float([9,2,7,7],1)
+b=Float([2,3,4,5],1,"-")
+c=a+b
+assert str(c)=="+693.2"
+
+print("\naddition")
+a=Float([9,2,7,7],1,"-")
+b=Float([2,3,4,5],1)
+c=a+b
+assert str(c)=="-693.2"
+
+print("\naddition")
+
+
+
